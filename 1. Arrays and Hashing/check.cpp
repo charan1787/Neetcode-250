705e5179d1dcb0fba3523a25b48c823df68d7b7b
Charan Shankar <83334298+charan1787@users.noreply.github.com>
2025-05-11 19:17:32 +0100

    Create check.cpp

---
+++ 1. Arrays and Hashing/check.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int hasha[26]={0};
+        //int hashb[26]={0};
+        if(s.size()!=t.size()){
+            return false;
+        }
+        for(char c:s){
+            hasha[c-'a']++;
+        }
+
+        for(char c:t){
+            hasha[c-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(hasha[i]!=0) return false;
+        }
+
+        return true;
+    }
+};

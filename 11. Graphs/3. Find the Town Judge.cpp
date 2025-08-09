04a17f95126ea085b412a60811462c98c5275a24
Charan Shankar <83334298+charan1787@users.noreply.github.com>
2025-08-09 13:57:20 +0100

    9th Aug - Easy

---
+++ 11. Graphs/3. Find the Town Judge.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> trusts(n+1,0);
+        vector<int> trustedby(n+1,0);
+
+        for(int i = 0;i<trust.size();i++){
+            trusts[trust[i][0]]++;
+            trustedby[trust[i][1]]++;
+        }
+
+        for(int i = 1;i<=n;i++){
+            if(trusts[i] == 0 && trustedby[i] == n-1){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
+
+// Simple vector operation
+
+        // vector<int> trusts(n+1,0);
+       //  vector<int> trustedby(n+1,0);
+				
+			// 	run a loop to store the numbers
+				
+				// then check with for loop for judge condition
+
+// https://leetcode.com/problems/find-the-town-judge/
